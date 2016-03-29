727d8c66094b0902a07a27335eec2f7527492c7b
Yoshie <yoshiemuranaka@gmail.com>
2016-03-29 13:31:00 -0400

    working on header animation

--- js/interactions.js
+++ js/interactions.js
@@ -10,6 +10,7 @@ Holly.Interactions = {
 		jQuery('.hamburger-menu').bind('click', this.menu);
 		jQuery('.menu-icon.close').bind('click', this.menu);
 		jQuery('.main-navigation .overlay').bind('click', this.menu);
+		jQuery('window').bind('scroll', this.animateHeader);
 	},
 	
 	menu: function() {
@@ -20,6 +21,23 @@ Holly.Interactions = {
 		//removing empty p and br tags
 		jQuery('.site-main p:empty').remove();
 		jQuery('.site-main br').remove();		
+	},
+
+	animateHeader: function() {
+		console.log('yo');
+		var distanceY = window.pageYOffset || document.documentElement.scrollTop;
+		var shrinkOn = 150;
+		var header = jQuery('.site-header');
+
+		if(small === false) {
+			if(distanceY > shrinkOn) {
+				header.addClass('smaller');
+			} else {
+				if(header.hasClass('smaller')) {
+					header.removeClass('smaller');
+				}
+			}
+		}     
 	}
 };
 
